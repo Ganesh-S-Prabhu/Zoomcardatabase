fd5a51103acd9e744d1bbdcd7fec951aa254e51a
Aishwarya P <93313435+Aishwaryapatil1@users.noreply.github.com>
2022-03-31 13:53:46 +0530

    register and login done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -39,14 +39,6 @@ const login = async (req, res) => {
             return res.status(400).send("Wrong Email or Password")
         }
 
-        //if email exists, check password;
-        const match = user.checkPassword(req.body.password)
-
-        // if it doesn't match
-        if(!match){
-            return res.status(400).send({message : "Wrong Email or Password"})
-        }
-
         // if it matches
         const token = generateToken(user)
         return res.status(200).send({user, token});
