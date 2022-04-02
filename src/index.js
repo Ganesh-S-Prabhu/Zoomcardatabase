243e939ede14b95cbdf8f9992d159e17b0209fb3
Ganesh-S-Prabhu <ganeshsprabhu2000@gmail.com>
2022-04-02 10:48:03 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const express = require("express");
  const bookingcontroller =require("./controllers/booking.controller")
  const carscontroller=require("./controllers/carscollection.controller")
  const user=require("./controllers/userdetails.controller")
-
+const cors=require("cors");
 const app = express();
 
+app.use(cors({
+  origin:"http://127.0.0.1:5500"
+}));
 app.use(express.json());
 
 app.use("/users",user);
